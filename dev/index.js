8200b18696f97f9445783ed206c1985f88e26647
Vladimir Maxymenko <462960@gmail.com>
2016-10-27 17:23:56 +0300

    Works. app.js and components.js are separated files

--- dev/index.js
+++ dev/index.js
@@ -18,7 +18,7 @@ import {hasHistory} from 'react-router';
 
 //import './sass/styles';
 require('./sass/styles');
-// import { Home, Stuff, Contact} from './component/components';
+import { Home, Stuff, Contact} from './component/components';
  import { App } from './component/app';
 //import Button from './button';
 //import {Button} from './button';
@@ -33,55 +33,55 @@ require('./sass/styles');
       IndexLink,
       Link } = ReactRouter;
 
-      var Home = React.createClass({
-  render: function() {
-      return (
-        <div>
-          <h2>HELLO</h2>
-          <p>Cras facilisis urna ornare ex volutpat, et
-          convallis erat elementum. Ut aliquam, ipsum vitae
-          gravida suscipit, metus dui bibendum est, eget rhoncus nibh
-          metus nec massa. Maecenas hendrerit laoreet augue
-          nec molestie. Cum sociis natoque penatibus et magnis
-          dis parturient montes, nascetur ridiculus mus.</p>
+//       var Home = React.createClass({
+//   render: function() {
+//       return (
+//         <div>
+//           <h2>HELLO</h2>
+//           <p>Cras facilisis urna ornare ex volutpat, et
+//           convallis erat elementum. Ut aliquam, ipsum vitae
+//           gravida suscipit, metus dui bibendum est, eget rhoncus nibh
+//           metus nec massa. Maecenas hendrerit laoreet augue
+//           nec molestie. Cum sociis natoque penatibus et magnis
+//           dis parturient montes, nascetur ridiculus mus.</p>
   
-          <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
-        </div>
-      );
-    }
-});
+//           <p>Duis a turpis sed lacus dapibus elementum sed eu lectus.</p>
+//         </div>
+//       );
+//     }
+// });
 
-var Contact = React.createClass({
-  render: function() {
-      return (
-        <div>
-          <h2>GOT QUESTIONS?</h2>
-          <p>The easiest thing to do is post on
-          our <a href="http://forum.kirupa.com">forums</a>.
-          </p>
-        </div>
-      );
-    }
-});
+// var Contact = React.createClass({
+//   render: function() {
+//       return (
+//         <div>
+//           <h2>GOT QUESTIONS?</h2>
+//           <p>The easiest thing to do is post on
+//           our <a href="http://forum.kirupa.com">forums</a>.
+//           </p>
+//         </div>
+//       );
+//     }
+// });
  
-var Stuff = React.createClass({
-  render: function() {
-      return (
-        <div>
-          <h2>STUFF</h2>
-          <p>Mauris sem velit, vehicula eget sodales vitae,
-          rhoncus eget sapien:</p>
-          <ol>
-            <li>Nulla pulvinar diam</li>
-            <li>Facilisis bibendum</li>
-            <li>Vestibulum vulputate</li>
-            <li>Eget erat</li>
-            <li>Id porttitor</li>
-          </ol>
-        </div>
-      );
-    }
-});
+// var Stuff = React.createClass({
+//   render: function() {
+//       return (
+//         <div>
+//           <h2>STUFF</h2>
+//           <p>Mauris sem velit, vehicula eget sodales vitae,
+//           rhoncus eget sapien:</p>
+//           <ol>
+//             <li>Nulla pulvinar diam</li>
+//             <li>Facilisis bibendum</li>
+//             <li>Vestibulum vulputate</li>
+//             <li>Eget erat</li>
+//             <li>Id porttitor</li>
+//           </ol>
+//         </div>
+//       );
+//     }
+// });
  
 // var App = React.createClass({
 //   render: function() {
